aba9cfc1d694d3ba547dd1c1f1b9031f5abb9dcd
Rakaneth <rakaneth@gmail.com>
2019-08-14 14:11:49 -0400

    refactor entity

--- entity.lisp
+++ entity.lisp
@@ -5,26 +5,29 @@
     :initarg :x 
     :accessor entity/x)
   (y 
-    :initarg :y 
-    :accessor entity/y)
+   :initarg :y 
+   :accessor entity/y)
   (char 
-    :initarg :char 
-    :accessor entity/char)
+   :initarg :char 
+   :accessor entity/char)
   (color
-    :initarg :color 
-    :accessor entity/color
-    :initform (blt:yellow))))
+   :initarg :color 
+   :accessor entity/color
+   :initform (blt:yellow))))
 
-(defmethod move ((e entity) dx dy)
-  (incf (entity/x e) dx)
-  (incf (entity/y e) dy))
-
-(defmethod move-coord ((e entity) coord)
-  (move e (car coord) (cdr coord)))
+(defmethod pos ((e entity))
+  (cons (entity/x e) (entity/y e)))
 
 (defmethod move-to ((e entity) x y)
   (setf (entity/x e) x)
-  (setf (entity/y e) y))
+  (setf (entity/y e) y)
+  (pos e))
+
+(defmethod move-by ((e entity) delta)
+  (let* ((cur-pos (pos e))
+         (new-pos (translate-coord cur-pos delta)))
+    (move-to e (car new-pos) (cdr new-pos))))
+
 
 (defmethod draw ((e entity))
   (with-slots (x y char color) e
